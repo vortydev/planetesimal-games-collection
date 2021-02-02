88cfc05a2e725199bcfc46bdca3950ce4f2c0766
vortydev <etienne.dmenard@gmail.com>
2021-02-01 22:14:54 -0500

    Added credits for the collection

--- Assets/Scripts/CreditsPage.cs
+++ Assets/Scripts/CreditsPage.cs
@@ -10,24 +10,33 @@ public class CreditsPage : MonoBehaviour
     [Header("Buttons")]
     [SerializeField] Button digimechButton;
     [SerializeField] Button sproutButton;
+    [SerializeField] Button collectionButton;
 
     [Header("Credits")]
     [SerializeField] GameObject digimechCredits;
     [SerializeField] GameObject sproutCredits;
+    [SerializeField] GameObject collectionCredits;
 
     private void Awake()
     {
-        digimechButton.interactable = false;
+        collectionButton.interactable = false;
         sproutCredits.SetActive(false);
+        digimechCredits.SetActive(false);
     }
 
     public void OnDigimechClick()
     {
+        // digimech
         digimechButton.interactable = false;
         digimechCredits.SetActive(true);
 
+        // sprout
         sproutButton.interactable = true;
         sproutCredits.SetActive(false);
+
+        // collection
+        collectionButton.interactable = true;
+        collectionCredits.SetActive(false);
 
         menu.PlayClickSound(0);
     }
@@ -39,6 +48,23 @@ public class CreditsPage : MonoBehaviour
 
         sproutButton.interactable = false;
         sproutCredits.SetActive(true);
+
+        collectionButton.interactable = true;
+        collectionCredits.SetActive(false);
+
+        menu.PlayClickSound(0);
+    }
+
+    public void OnCollectionClick()
+    {
+        digimechButton.interactable = true;
+        digimechCredits.SetActive(false);
+
+        sproutButton.interactable = true;
+        sproutCredits.SetActive(false);
+
+        collectionButton.interactable = false;
+        collectionCredits.SetActive(true);
 
         menu.PlayClickSound(0);
     }
